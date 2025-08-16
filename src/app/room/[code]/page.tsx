7e70fdfa3cd19ec838b4971c190db63e8d6b2257
ayberkkk <ayberktiglii@gmail.com>
2025-08-16 19:31:09 +0300

    fix: update page props type for Next.js App Router

--- src/app/room/[code]/page.tsx
+++ src/app/room/[code]/page.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import { Metadata } from 'next';
 import RoomDetailClient from '@/app/room/[code]/RoomDetailClient';
 
-interface PageProps {
-  params: {
-    code: string;
-  };
-}
+type Props = {
+  params: { code: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   return {
     title: `Room ${params.code} - Plan/Lab`,
   };
 }
 
-export default async function RoomDetailPage({ params }: PageProps) {
+export default function RoomDetailPage({ params }: Props) {
   return <RoomDetailClient code={params.code} />;
 }
